ba0e83a574d7d9858cc451271304cb34dbf405f2
achavazza <alejandrochavazza@gmail.com>
2014-12-10 18:07:56 -0300

    clearfix mixin

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <title>Quickstarter</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <meta name="description" content="Construye tu proximo proyecto HTML5 responsive adaptado a tablets y móbiles en minutos - Sistema de Grid HTML5 "  />
-     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="http://achavazza.github.io/getquickstarter/favicon.ico">
-    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300' rel='stylesheet' type='text/css'>
-    <!--<link rel="stylesheet" href="css/quickstarter.min.css" />-->
-    <link rel="stylesheet" href="http://achavazza.github.io/getquickstarter/css/quickstarter.min.css" />
-</head>
+<?php include('inc/head.php') ?>
+<body>
+	<div id="container">
+		<aside id="aside">
+			<a href="/" class="logo"></a>
+			<ul class="stack">
+				<li><a href="#">Introduction</a></li>
+				<li><a href="#">The grid</a></li>
+				<li><a href="#">Toolbox</a></li>
+				<li><a href="#">Responsive</a></li>
+				<li><a href="#">Forms</a></li>
+				<li><a href="#">Buttons</a></li>
+				<li><a href="#">Base</a></li>
+			</ul>
+		</aside>
+		<div class="content">
+			<div class="grid">
+			<div class="row">
+			<div class="quad-4">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</body>
+<?php /* 
 <body>
 	<div class="jumbotron">
 		<div class="grid">
@@ -71,4 +89,5 @@
 		</div>
 	</div>
 </body>
+*/ ?>
 </html>
