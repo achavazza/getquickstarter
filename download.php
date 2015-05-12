97b24a0b5b7b442df2f1cc0989c32c90b7b38fd5
achavazza <alejandrochavazza@gmail.com>
2015-05-11 22:05:34 -0300

    download section

--- download.php
+++ download.php
@@ -4,22 +4,30 @@
     <div class="row">
         <div id="sidebar" class="quad-1">
             <ul class="stack">
-                <li><a href="#forms">Forms</a></li>
-                <li><a href="#form">Form</a></li>
-                <li><a href="#form-horizontal">Form Horizontal</a></li>
-                <li><a href="#input-append">Input append</a></li>
+                <li><a href="#download">Download</a></li>
+                <li><a href="#psd-template">PSD Template</a></li>
             </ul>
         </div>
         <div class="quad-3">
             <section id="download">
-                
+                <h2 class="h1 title">Download</h2>
+                <p>Download standalone .zip, ready to go!</p>
+                <a href="https://github.com/achavazza/quickstarter/archive/master.zip" class="btn btn-primary uppercase" target="_blank">
+                    Download .zip
+                </a>
             </section>
             <section id="psd-template">
-                
+                <h2 class="h1 title">PSD template</h2>
+                <p>A blank Sheet ready to design</p>
+                <a href="https://github.com/achavazza/getquickstarter/download/QS-grid.zip" class="btn btn-primary uppercase" target="_blank">
+                    Download PSD Template
+                </a>
             </section>
+            <?php /* 
             <section id="psd-ui">
                 
             </section>
+            */ ?>
         </div>
     </div>
 </div>
